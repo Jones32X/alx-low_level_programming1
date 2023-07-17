5f5fd30ae001c3cf257c20b558a296aea15f61df
Jones32X <jonespedro.Nezingu.com>
2023-07-17 04:48:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#inculde <stdio.h>
+/**
+ * main - Program that uses the put function to print a line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
