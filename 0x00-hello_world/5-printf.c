02c01301d6427ef7c86544d21fb9fba6bd58ca8a
Jones32X <jonespedro.Nezingu.com>
2023-07-17 05:47:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that uses the print f function to print a line
+ * Return:0
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n ")
+		return (0);
+}
