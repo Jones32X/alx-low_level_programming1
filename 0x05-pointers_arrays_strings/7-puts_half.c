355222c8783203a21dc8bc83eb2b82404a0ec0b4
Jones32X <jonespedro.Nezingu.com>
2023-07-25 21:53:02 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @S: string
+ * Return: length
+ */
+
+int _strlen(char *S)
+{
+	int longi = 0;
+
+	while (*S != 0 '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
