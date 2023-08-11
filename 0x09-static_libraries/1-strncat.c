203cca5ce3a847cae9b0752db0f8ddcd00f6ca43
Jones32X <jonespedro.Nezingu.com>
2023-08-10 22:33:39 -0700

    fixed and added  _strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,3 +1,32 @@
 #include "main.h"
 
+/**
+ * _strncat - combines to strings
+ * with n bytes through src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+
+	while (y < n && src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
