76ca0c4629b7f27f94b80421939a71b0e05738f6
Jones32X <jonespedro.Nezingu.com>
2023-08-14 03:08:18 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program name
+ * @argc: counts  arguments
+ * @argv: arguments array
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
